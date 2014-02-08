020ed764d50e64f3afc44fad8942d5c5cb1029f8
thobson <thobson@gmail.com>
2014-02-08 14:29:59 -0800

    Mongo setup without hard-coded creeds

--- src/main/java/org/challenge/DataConfig.java
+++ src/main/java/org/challenge/DataConfig.java
@@ -2,23 +2,21 @@ package org.challenge;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.authentication.UserCredentials;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.mongodb.MongoDbFactory;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
 
-import com.mongodb.Mongo;
-import com.mongodb.ServerAddress;
+import com.mongodb.MongoURI;
 
+@Profile("heroku")
 @Configuration
 public class DataConfig {
 
 	@Bean
 	public MongoDbFactory mongoDbFactory() throws Exception {
-		UserCredentials userCredentials = new UserCredentials("dbuser", "dbpass");
-		ServerAddress serverAddress = new ServerAddress("troup.mongohq.com", 10087);
-		return new SimpleMongoDbFactory(new Mongo(serverAddress), "app22039103",
-				userCredentials);
+		MongoURI mongoURI = new MongoURI(System.getenv("MONGOHQ_URL"));
+		return new SimpleMongoDbFactory(mongoURI);
 	}
 
 	@Bean
