ac882f35c683fcbbc24bcb13fe756c1357f75237
Shrisha Radhakrishna <shrisha@gmail.com>
2014-02-08 23:08:41 -0500

    wire up createUI

--- src/main/resources/public/js/controllers.js
+++ src/main/resources/public/js/controllers.js
@@ -2,7 +2,7 @@
 "use strict";
 
 angular.module( "app.controllers", [ ] )
-    .controller( "ChallengeController", function( $scope, $location, $routeParams, $http) {
+    .controller( "ChallengeController", function( $scope, $location, $routeParams, $http, $modal) {
 
         $scope.fetchChallenges = function() {
             $http.get('challenge').success(function(challenges){
@@ -12,17 +12,35 @@ angular.module( "app.controllers", [ ] )
 
         console.log("feching challenges");
         $scope.fetchChallenges();
+
+        $scope.open = function (modalName) {
+            var modalInstance = $modal.open({
+                templateUrl: 'partials/createChallenge.html',
+                controller: 'CreateChallengeController'
+//                resolve: {
+//                    'items': function() { return $scope.items; }
+//                }
+            });
+            console.log('modal opened');
+            modalInstance.result.then(function (response) {
+//                $scope.selected = response;
+                console.log(response);
+            }, function () {
+                console.log('Modal dismissed at: ' + new Date());
+            });
+        };
     })
-    .controller( "CreateChallengeController", function( $scope, $location, $routeParams, $http) {
+    .controller( "CreateChallengeController", function( $scope, $location, $routeParams, $http, $modalInstance) {
 
         $scope.createChallenge = function(description) {
             $http.post('challenge',{'description':description}).success(function(id){
                 console.log("id="+id);
+                $modalInstance.close(id);
             });
         };
 
         $scope.cancel = function(){
-//            $modalInstance.dismiss('cancel');
+            $modalInstance.dismiss('cancel');
 
         }
         console.log("create challenges");
